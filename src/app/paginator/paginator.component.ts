3af1da5515c848f5ad7ce112edad33a12ca0e883
Kevin Tourret <kevin@drosalys.fr>
2022-05-11 15:44:28 +0200

    @Output examples - working button pagination

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {HydraViewContent} from "../../models/steamish/i-api-result";
 
 @Component({
@@ -11,8 +11,18 @@ export class PaginatorComponent {
   @Input()
   hydraView: HydraViewContent|undefined;
 
-  updateGamePage(url: string|undefined): void {
+  /**
+   * Ici on voit remonter au component mère, l'url sur laquelle l'utilisateur vient de cliquer
+   * Donc le type entre chevrons de notre EventEmitter est de type string
+   */
+  @Output()
+  eventEmitterUrlClicked: EventEmitter<string> = new EventEmitter<string>();
 
+  emitUrl(url: string|undefined): void {
+    console.log(url);
+    // L'évènement émis est du même type que celui déclaré dans notre EventEmitter, ici string
+    // donc la fonction emit de l'EventEmitter attend un paramètre de type string
+    this.eventEmitterUrlClicked.emit(url);
   }
 
 }
